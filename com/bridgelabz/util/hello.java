5a3589f6249bebcfd105f648837edb91e41d461f
qaisbubere <qaisbubere1995@gmail.com>
2017-08-06 16:57:07 +0530

    first commit

---
+++ com/bridgelabz/util/hello.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+*  Purpose: Determines how many prime numbers exist between 0 to 100000.
+	    find out, how many numbers are palindrome among those prime numbers.
+ *
+ *  @author  Qais Bubere
+ *  @version 1.0
+ *  @since   06-08-2017
+ *
+ ******************************************************************************/
+
+/*
+ * JAVA STYLE GUIDE - PACKAGE NAMING STYLE
+ */
+
+package com.bridgelabz.util;
+
+import java.io.*;
+
+class hello
+{
+	public static void main(String args[])
+{
+	System.out.println("hello world");
+}
+}
